ed1a442fbe8deecb14b660e7f4a52346d6aabbf7
IGITANGAZA23 <igitangazanobleprince@gmail.com>
2025-10-06 09:33:33 -0800

    Feat complexity analysis program

---
+++ complexity.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int count = 0 ;
+	for(int i = 0; i<10 ;i +1){
+		for (int j =0 ; j< 10 ; j++){
+			count ++ ;
+		}
+	}
+	cout<<count<<endl ;
+	return 0 ;
+}
